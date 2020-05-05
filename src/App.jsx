5ee59bd94f6e6841097b506048e1eb6d1345e837
abrar063 <abrar063@uottawa.ca>
2020-05-05 14:36:54 -0400

    Added square template for board in jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Square from './codenames/Square';
+
+
+function App() {
+  return (
+    <Square/>
+
+    
+  );
+}
+
+export default App;
