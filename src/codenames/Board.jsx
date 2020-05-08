05db2ddfdea02d8936011cdb70faf00b06f75d5e
abrar063 <abrar063@uottawa.ca>
2020-05-08 16:35:42 -0400

    Added a word bank and switched buttons to flex display

--- src/codenames/Board.jsx
+++ src/codenames/Board.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import {colors, words} from './constants';
 
-const colors = ["red", "red", "red", "red", "red", "red", "red", "red", "aqua","aqua","aqua","aqua","aqua","aqua","aqua","aqua","aqua","black", "grey","grey","grey","grey","grey","grey","grey"]
 
 const Code = styled.button`
   background: ${props => props.bg};
   color: ${props => (props.bg == "red" || props.bg == "aqua") ? "black" : "white"};
-  font-size: 1em;
+  font-size: 0.9em;
   margin: 1em;
-  padding: 0.25em 1em;
+  display: flex;
+  align-items: center;
+  justify-content:center;
   border: 2px solid black;
   border-radius: 3px;
+  width: 100px;
+  height: 100px;
 `;
 
 function shuffleArray(array) {
@@ -23,43 +27,44 @@ function shuffleArray(array) {
 
 class Board extends React.Component{    
     renderBoard(i){
-       shuffleArray(colors)
+       shuffleArray(colors) 
+       shuffleArray(words)
     return(
     <div>
-        <div>
-        <Code bg = {colors[0]}>Word</Code>
-        <Code bg = {colors[1]}>Word</Code>
-        <Code bg = {colors[2]}>Word</Code>
-        <Code bg = {colors[3]}>Word</Code>
-        <Code bg = {colors[4]}>Word</Code>
+        <div> 
+        <Code bg = {colors[0]}>{words[0]}</Code>
+        <Code bg = {colors[1]}>{words[1]}</Code>
+        <Code bg = {colors[2]}>{words[2]}</Code>
+        <Code bg = {colors[3]}>{words[3]}</Code>
+        <Code bg = {colors[4]}>{words[4]}</Code>
         </div>
         <div>
-        <Code bg = {colors[5]}>Word</Code>
-        <Code bg = {colors[6]}>Word</Code>
-        <Code bg = {colors[7]}>Word</Code>
-        <Code bg = {colors[8]}>Word</Code>
-        <Code bg = {colors[9]}>Word</Code>
+        <Code bg = {colors[5]}>{words[5]}</Code>
+        <Code bg = {colors[6]}>{words[6]}</Code>
+        <Code bg = {colors[7]}>{words[7]}</Code>
+        <Code bg = {colors[8]}>{words[8]}</Code>
+        <Code bg = {colors[9]}>{words[9]}</Code>
         </div>
         <div>
-        <Code bg = {colors[10]}>Word</Code>
-        <Code bg = {colors[11]}>Word</Code>
-        <Code bg = {colors[12]}>Word</Code>
-        <Code bg = {colors[13]}>Word</Code>
-        <Code bg = {colors[14]}>Word</Code>
+        <Code bg = {colors[10]}>{words[10]}</Code>
+        <Code bg = {colors[11]}>{words[11]}</Code>
+        <Code bg = {colors[12]}>{words[12]}</Code>
+        <Code bg = {colors[13]}>{words[13]}</Code>
+        <Code bg = {colors[14]}>{words[14]}</Code>
         </div>
         <div>
-        <Code bg = {colors[15]}>Word</Code>
-        <Code bg = {colors[16]}>Word</Code>
-        <Code bg = {colors[17]}>Word</Code>
-        <Code bg = {colors[18]}>Word</Code>
-        <Code bg = {colors[19]}>Word</Code>
+        <Code bg = {colors[15]}>{words[15]}</Code>
+        <Code bg = {colors[16]}>{words[16]}</Code>
+        <Code bg = {colors[17]}>{words[17]}</Code>
+        <Code bg = {colors[18]}>{words[18]}</Code>
+        <Code bg = {colors[19]}>{words[19]}</Code>
         </div>
         <div>
-        <Code bg = {colors[20]}>Word</Code>
-        <Code bg = {colors[21]}>Word</Code>
-        <Code bg = {colors[22]}>Word</Code>
-        <Code bg = {colors[23]}>Word</Code>
-        <Code bg = {colors[24]}>Word</Code>
+        <Code bg = {colors[20]}>{words[20]}</Code>
+        <Code bg = {colors[21]}>{words[21]}</Code>
+        <Code bg = {colors[22]}>{words[22]}</Code>
+        <Code bg = {colors[23]}>{words[23]}</Code>
+        <Code bg = {colors[24]}>{words[24]}</Code>
         </div>
     </div>);
 
